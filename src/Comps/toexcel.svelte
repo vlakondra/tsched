675361eac76b90dbebb5d656aac11e4d35a9c300
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-06-25 17:03:56 +0500

    did button toExcel

--- src/Comps/toexcel.svelte
+++ src/Comps/toexcel.svelte
@@ -1,12 +1,23 @@
 <script>
     import { client_width } from "./store.js";
+    import { scheddata, sched_data_loaded } from "./store";
     import Fa from "svelte-fa";
     import { faFileExcel } from "@fortawesome/free-solid-svg-icons";
 </script>
 
-<button class="button is-primary  is-medium">
+<button
+    disabled={Object.keys($scheddata).length > 0 ? false : true}
+    class="button is-info  is-normal to-center"
+>
     <span class="icon">
-        <Fa icon={faFileExcel} color="wheat" size="1x" />
+        <Fa icon={faFileExcel} color="white" size="1x" />
     </span>
     <span>Сохранить в формате Excel</span>
 </button>
+
+<style>
+    .to-center {
+        display: block;
+        margin: 30px auto;
+    }
+</style>
